6c677f52b841988c66b3ca75748c75346b55f6f9
Nana Adjei Manu <nkwadwo06@gmail.com>
2024-04-15 12:32:05 +0100

    feat: add yaml parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
+import { readFileSync } from "node:fs"
+import { parse } from "yaml"
 import { Toggle, ToggleStatus, ToggleStatuses } from "./types";
 
 export function isOn(status: ToggleStatus): boolean {
   return status === "on";
 }
+
+type fileType = "yml" | "json"
+
+class FileParser {
+  _filename: string = ""
+  _type: fileType = "yml"
+  constructor(filename: string, type: fileType = "yml") {
+    this._filename = filename
+    this._type = type
+  }
+}
+
+
+class YamlParser extends FileParser {
+  _yamlFlags = {}
+  readFile() {
+    const yamlObj = readFileSync("./src/file.yml", "utf8")
+    this._yamlFlags = yamlObj
+    console.log({ yamlObj })
+    return this
+  }
+
+  getFlags() {
+    return this._yamlFlags
+  }
+}
+
+const yamlFlags = new YamlParser("file.yml").readFile()
+
+console.log(yamlFlags.getFlags())
 
 export class LaunchLever {
   _flags: Toggle[] = [];
@@ -10,11 +42,11 @@ export class LaunchLever {
     this._flags = flags;
   }
 
-  get toggles(): Toggle[] {
+  toggles(): Toggle[] {
     return this._flags;
   }
 
-  get flags() {
+  flags() {
     let toggles: ToggleStatuses = {};
     for (let t of this._flags) {
       toggles[t.name] = t.status;
